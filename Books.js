b01280dacd867261ea7dd0470447cc59ba475cbb
Fizza-Ashfaq <fizza0849@gmail.com>
2024-10-29 10:55:09 +0500

    created array for data

--- Books.js
+++ Books.js
@@ -1,1 +1,19 @@
 const app=express();
+const books=[
+    {  id:1,
+       title: "The Catcher in the Rye",
+       author: "J.D. Salinger",
+       price: 19.99
+    },
+    {   id:2,
+        title: "To Kill a Mockingbird",
+        author: "Harper Lee",
+        price: 12.99
+    },
+    {
+        id:3,
+        title: "1984",
+        author: "George Orwell",
+        price: 14.99
+    }
+]
